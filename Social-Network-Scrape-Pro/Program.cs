2ccd47363eb5a34dab84583ab9ab90f4455a4c84
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-02-03 08:59:09 +0800

    Add project files.

---
+++ Social-Network-Scrape-Pro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Social_Network_Scrape_Pro
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
